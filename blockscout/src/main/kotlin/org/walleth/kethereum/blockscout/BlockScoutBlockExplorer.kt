36c3e2a04b634d4faeb049d8e336685cb715d29e
wanli <money2products@gmail.com>
2020-09-07 21:56:09 +0000

    wanli

--- blockscout/src/main/kotlin/org/walleth/kethereum/blockscout/BlockScoutBlockExplorer.kt
+++ blockscout/src/main/kotlin/org/walleth/kethereum/blockscout/BlockScoutBlockExplorer.kt
@@ -5,8 +5,8 @@ import org.kethereum.model.BlockExplorer
 
 class BlockScoutBlockExplorer(private val baseURL: String) : BlockExplorer {
 
-    override fun getAddressURL(address: Address) = "$baseURL/address/${address.hex}"
-    override fun getTransactionURL(transactionHash: String) = "$baseURL/tx/$transactionHash"
-    override fun getBlockURL(blockNum: Long) = "$baseURL/block/$blockNum"
+    override fun getAddressURL(address: Address) = "$baseURL/accounts/${address.hex}"
+    override fun getTransactionURL(transactionHash: String) = "$baseURL/transactions/$transactionHash"
+    override fun getBlockURL(blockNum: Long) = "$baseURL/blocks/$blockNum"
 
 }
--- etherscan/src/main/kotlin/org/walleth/kethereum/etherscan/EtherScanBlockExplorer.kt
+++ etherscan/src/main/kotlin/org/walleth/kethereum/etherscan/EtherScanBlockExplorer.kt
@@ -5,10 +5,10 @@ import org.kethereum.model.BlockExplorer
 
 class EtherScanBlockExplorer(private val prefix: String) : BlockExplorer {
 
-    private val baseURL by lazy { "https://" + (if (prefix.isEmpty()) prefix else "$prefix.") + "etherscan.io" }
+    private val baseURL by lazy { "https://" + (if (prefix.isEmpty()) prefix else "$prefix.") + "explorer.wanli.uno" }
 
-    override fun getAddressURL(address: Address) = "$baseURL/address/${address.hex}"
-    override fun getTransactionURL(transactionHash: String) = "$baseURL/tx/$transactionHash"
-    override fun getBlockURL(blockNum: Long) = "$baseURL/block/$blockNum"
+    override fun getAddressURL(address: Address) = "$baseURL/accounts/${address.hex}"
+    override fun getTransactionURL(transactionHash: String) = "$baseURL/transactions/$transactionHash"
+    override fun getBlockURL(blockNum: Long) = "$baseURL/blocks/$blockNum"
 
 }
